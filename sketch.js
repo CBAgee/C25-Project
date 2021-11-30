8500765d0946a5d3d3fd6c7955b38f3afea5a526
CBAgee <88510934+CBAgee@users.noreply.github.com>
2021-11-30 17:03:23 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,9 +14,9 @@ var arrows = [];
 var playerArrows = [];
 
 function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+  backgroundImg = loadImage("background.png");
+  baseimage = loadImage("base.png");
+  playerimage = loadImage("player.png");
 }
 
 function setup() {
